85946fb1ac35a06115c9095a02a23955fe7f91b6
vel <velmurugan1124@gmail.com>
2022-03-31 17:47:20 +0530

    Refactoring

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const { reminderHandler } = require('./handlers/reminderHandler');
 const { checkinHandler } = require('./handlers/checkinHandler');
 
 // Routes
-// const reminderRoute = require('./routes/reminder');
+const reminderRoute = require('./routes/reminder');
 
 
 app.use(morgan('dev'))
@@ -27,33 +27,36 @@ app.get('/', async (req, res) => {
 
 })
 
-app.get('/test', async (req, res) => {
-    let event = await getEvent(req.query.event_id)
-    console.log('event', event);
-    let event_type = req.query.event_type
-    let result = await findTokenFromDB(req.query.bot_id)
-    console.log('REslt', result);
-    let token = result.rows[0]
-    let bot_id = req.query.bot_id
+app.use('/incoming', reminderRoute);
 
-    if (req.query.schedule_type === 'invitation') {
 
-        await invitationHandler(token, event_type, event, bot_id)
-    }
-    if (req.query.schedule_type === 'fomo_reminder') {
-        await fomoHandler(token, event, bot_id)
-    }
-    if (req.query.schedule_type === 'reminder') {
-        await reminderHandler(token, event.attendees, bot_id)
-    }
+// app.get('/test', async (req, res) => {
+//     let event = await getEvent(req.query.event_id)
+//     console.log('event', event);
+//     let event_type = req.query.event_type
+//     let result = await findTokenFromDB(req.query.bot_id)
+//     console.log('REslt', result);
+//     let token = result.rows[0]
+//     let bot_id = req.query.bot_id
 
-    if (req.query.schedule_type === 'checkin') {
-        console.log('Checkin');
-        await checkinHandler(token, event_type, event, bot_id)
-    }
+//     if (req.query.schedule_type === 'invitation') {
 
-    res.json({ 'message': 'Succes' }).status(200)
-})
+//         await invitationHandler(token, event_type, event, bot_id)
+//     }
+//     if (req.query.schedule_type === 'fomo_reminder') {
+//         await fomoHandler(token, event, bot_id)
+//     }
+//     if (req.query.schedule_type === 'reminder') {
+//         await reminderHandler(token, event.attendees, bot_id)
+//     }
+
+//     if (req.query.schedule_type === 'checkin') {
+//         console.log('Checkin');
+//         await checkinHandler(token, event_type, event, bot_id)
+//     }
+
+//     res.json({ 'message': 'Succes' }).status(200)
+// })
 
 app.post('/test', async (req, res) => {
     console.log('REQ', req);
--- src/routes/reminder.js
+++ src/routes/reminder.js
@@ -1,12 +1,47 @@
 const router = require('express').Router()
-const platform = require('../utils/platform.js');
 
+const { getEvent, findTokenFromDB } = require('../utils/db')
+
+// Import Handlers
+const { invitationHandler } = require('../handlers/invitationHandler');
+const { fomoHandler } = require('../handlers/fomoReminderHandler');
+const { reminderHandler } = require('../handlers/reminderHandler');
+const { checkinHandler } = require('../handlers/checkinHandler');
 
 /**
  * Authorize
  */
-router.get('/bulk_messaging', async (req, res) => {
-    console.log('');
+router.get('/schedule', async (req, res) => {
+
+    try {
+        let event = await getEvent(req.query.event_id)
+        let event_type = req.query.event_type
+        let result = await findTokenFromDB(req.query.bot_id)
+        let token = result.rows[0]
+        let bot_id = req.query.bot_id
+
+        if (req.query.schedule_type === 'invitation') {
+
+            await invitationHandler(token, event_type, event, bot_id)
+        }
+        if (req.query.schedule_type === 'fomo_reminder') {
+            await fomoHandler(token, event, bot_id)
+        }
+        if (req.query.schedule_type === 'reminder') {
+            await reminderHandler(token, event.attendees, bot_id)
+        }
+
+        if (req.query.schedule_type === 'checkin') {
+            console.log('Checkin');
+            await checkinHandler(token, event_type, event, bot_id)
+        }
+        res.json({ 'message': 'Succes' }).status(200)
+    } catch (error) {
+        console.error('Unable to handle Schedules');
+        console.error(error);
+        res.json({ 'message': 'Succes' }).status(500)
+
+    }
 })
 
 
